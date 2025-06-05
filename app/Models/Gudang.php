12e39f89ab5d94ccfbacdf44d40c4f60621f6624
Des Dulianto <desdulianto@gmail.com>
2025-06-05 18:30:51 +0700

    gudang resource

---
+++ app/Models/Gudang.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Gudang extends Model
+{
+    protected $table = 'gudangs';
+
+    protected $fillable = [
+        'nama',
+        'aktif',
+    ];
+
+    protected $casts = [
+        'aktif' => 'bool',
+    ];
+}

ec0c170261de095823e83086da06aa31c269a3ec
Des Dulianto <desdulianto@gmail.com>
2025-06-26 18:27:58 +0700

    testing

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -3,5 +3,14 @@
 test('the application returns a successful response', function () {
     $response = $this->get('/');
 
-    $response->assertStatus(200);
+    $response->assertStatus(404);
 });
+
+test('mengakses halaman admin, tanpa login akan di redirect ke halaman login',
+function () {
+    $response = $this->get('/admin');
+
+    $response->assertStatus(302);
+    $response->assertLocation('/admin/login');
+}
+);
